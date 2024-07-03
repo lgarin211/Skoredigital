2a3d58c6d22ffc82470fd4f3288392a135d7dc64
lgarin211 <lagustinus211@gmail.com>
2024-07-03 08:41:46 +0700

    modified:   juritgr/_penilaian.html 	modified:   juritgr/ganda.html 	modified:   juritgr/index.html 	modified:   juritgr/tunggal.html 	modified:   nilaitgr/view_ganda.php 	modified:   nilaitgr/view_regu.php 	modified:   nilaitgr/view_tunggal langsung.php 	modified:   nilaitgr/view_tunggal.php 	modified:   nilaitgr/view_tunggal_semua_juri.php 	modified:   nilaitgr/view_tunggal_sip.php 	modified:   wasjur/index.html 	modified:   wasjur/penilaian.html

--- nilaitgr/view_tunggal langsung.php
+++ nilaitgr/view_tunggal langsung.php
@@ -176,7 +176,7 @@ include "../backend/includes/connection.php";
 			<script type="text/javascript">
 				setInterval(function() {
 					$.ajax({
-						url: 'http://localhost/skordigital/juritgr/api.php',
+						url: 'https://demoskoredigital.garinpoin.com/juritgr/api.php',
 						data: {
 							'a': 'get_data_view_tunggal'
 						},
